6a770c65e7150e287c79ab437e1b9c67263fad18
Tanay Pratap <tapratap@microsoft.com>
2021-05-14 18:06:14 +0530

    problem statement

--- src/services/get-user.test.js
+++ src/services/get-user.test.js
@@ -6,22 +6,14 @@ jest.mock("axios");
 describe("get user service", () => {
     
 	test("should return user when API call is successful", async () => {
-		axios.get.mockResolvedValue({ data: { name: "Tanay", age: 30 } });
-		const user = await getUser();
-
-        expect(user).toEqual({ name: "Tanay", age: 30 });
+	
+        expect(true).toBe(false);
 
 	});
 
     test("should return errorMessage when API is call fails", async () => {
-        axios.get.mockRejectedValue({ isAxiosError: true, response: { data: { errorMessage: "user not found"}}});
-
-        axios.isAxiosError.mockImplementation((payload) => true)
-        
-            const user = await getUser();
-            expect(user).toEqual({ errorMessage: "user not found" });
-
-            expect(axios.isAxiosError).toBeCalledTimes(1)
+       
+        expect(true).toBe(false);
         
     })
 });
